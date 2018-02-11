a61e84fd061fef385616efd652b12685501ce914
maxjoerg <max.schleiwies@googlemail.com>
2018-02-11 21:52:30 +0100

    File Handling corrected for download and working directory

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,14 @@
-library(reshape2) 
+
 library(tidyr)
 library(dplyr)
-setwd("C://Users//Jörg//Documents//coursera//datascience//Kurs3_Woche4")
+## initial local directory
+##setwd("C://Users//J?rg//Documents//coursera//datascience//Kurs3_Woche4")
 
 filename <- "getdata_projectfiles_UCI HAR Dataset.zip" 
 
 ## Download and unzip the dataset: 
 if (!file.exists(filename)){ 
-    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip " 
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
    download.file(fileURL, filename, method="curl") 
 }   
 if (!file.exists("UCI HAR Dataset")) {  
